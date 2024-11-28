201d9edf365ea0c1d9e9c9890f3bc249532564e9
Carla Trujillo <29936457+Carliz@users.noreply.github.com>
2024-11-27 18:21:49 -0600

    ajuste para revisar soporte de webgl

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
 
+
+//para marcar el canvas donde trabajaremos
 const w = window.innerWidth;
 const h = window.innerHeight;
 
+//se crea la escena y se ponce color
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x000000);
 
+//creamos la camara y le ponemos la posición de Z para que se vea el cubo (0,0,0)
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 100);
 camera.position.z = 5;
 
@@ -32,6 +37,18 @@ function animate(){
     cube.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
-animate();
+//animate();
 
-renderer.render(scene, camera);
+//revisar si tiene compatibilidad con webGL2
+if(WebGL.isWebGL2Available()){
+    // Initiate function or other initializations here
+    animate();
+    renderer.render(scene, camera);
+}
+else
+{
+    const warning = WebGL.getWebGL2ErrorMessage();
+    document.getElementById('container').appendChild(warning);
+}
+//lo edité y puse dentro del if de webGL2 support
+//renderer.render(scene, camera);
